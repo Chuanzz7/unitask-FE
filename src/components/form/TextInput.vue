7895b129dce620b4bab339fe3847865264f2f043
ChuanATF <chngkahchuan@altecflex.com>
2024-12-04 11:09:31 +0800

    truncate listing

--- src/components/form/TextInput.vue
+++ src/components/form/TextInput.vue
@@ -25,8 +25,8 @@ function updateValue(value) {
       <input :disabled :type="type" :value="modelValue" @input="updateValue($event.target.value)"
              :class="[disabled? 'border-gray-50 bg-gray-50 '
              :'border-gray-300 bg-white '
-             ,'focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none ' +
-             'rounded-lg border border-solid  bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none ' +
+             ,'truncate focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none ' +
+             'rounded-lg border border-solid bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none ' +
              'transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none']"/>
     </div>
   </div>
--- src/components/small-list/Small-list.vue
+++ src/components/small-list/Small-list.vue
@@ -31,34 +31,30 @@ const routeToEdit = () => {
   ? 'bg-gray-200'
   : ''
   ,'flex-auto m-3 px-4 hover:bg-gray-200 rounded-lg']">
-    <ul class="flex flex-col pl-0 mb-0 rounded-lg">
-      <li class="relative flex justify-between px-2 py-2 pl-0  border-0 rounded-t-inherit text-inherit rounded-xl">
-
-        <div class="w-full flex justify-between" @click="routeToDetails">
-          <div class="flex flex-col">
-            <h6 class="mb-1 text-sm font-semibold leading-normal dark:text-white text-slate-700">
+      <div class="relative flex justify-between px-2 py-2 pl-0  border-0 rounded-t-inherit text-inherit rounded-xl">
+        <div class="w-[93%] flex justify-between" @click="routeToDetails">
+          <div class="flex truncate flex-col">
+            <h6 class="truncate mb-1 text-sm font-semibold leading-normal dark:text-white text-slate-700">
               {{ data.title }}</h6>
-            <span class="text-xs leading-tight dark:text-white dark:opacity-80">{{ data.description }}</span>
+            <span class="truncate text-xs leading-tight dark:text-white dark:opacity-80">
+              {{ data.description }}</span>
           </div>
-          <div
-              class="flex text-center align-middle items-center text-sm leading-normal  transition-all bg-transparent border-0 ease-in bg-150 rounded-lg shadow-none dark:text-white/80">
+          <div class="flex text-center align-middle items-center text-sm leading-normal  transition-all bg-transparent border-0 ease-in bg-150 rounded-lg shadow-none dark:text-white/80">
           <span
-              class="dark:text-white  ml-6 font-bold  uppercase
-              text-sm active:opacity-85 tracking-tight-rem bg-x-25 text-slate-700">
-            {{ data.code }}
-          </span>
+              class="dark:text-white ml-3 font-bold uppercase text-sm active:opacity-85 tracking-tight-rem bg-x-25 text-slate-700">
+            {{ data.code }}</span>
           </div>
         </div>
 
-        <div class="flex text-center align-middle items-center text-sm leading-normal  transition-all bg-transparent border-0 ease-in bg-150 rounded-lg shadow-none dark:text-white/80">
-          <button class="dark:text-white ml-3 font-bold cursor-pointer ease-in bg-150 text-sm active:opacity-85 hover:-translate-y-px tracking-tight-rem bg-x-25 text-slate-700"
-                  @click="routeToEdit" v-if="editable && updatePage">
-            <i class=" text-sm pi pi-pencil"></i>
+        <div
+            class="flex leading-normal">
+          <button
+              class="dark:text-white cursor-pointer ease-in bg-150 text-sm active:opacity-85 hover:-translate-y-px tracking-tight-rem bg-x-25 text-slate-700"
+              @click="routeToEdit" v-if="editable && updatePage">
+            <i class="text-sm pi pi-pencil"></i>
           </button>
         </div>
-
-      </li>
-    </ul>
+      </div>
   </div>
 </template>
 
