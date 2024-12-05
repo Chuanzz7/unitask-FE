95617d4a8b98bd232a9de650feccbd8cbef1ca17
ChuanATF <chngkahchuan@altecflex.com>
2024-12-05 15:13:41 +0800

    Color Picker

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import Toast from "vue-toastification";
 import router from "./router";
 
 import App from "./App.vue";
+import Vue3ColorPicker from "vue3-colorpicker";
+import "vue3-colorpicker/style.css";
+
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -15,5 +18,6 @@ const pinia = createPinia();
 app.use(router);
 app.use(Toast);
 app.use(pinia);
+app.use(Vue3ColorPicker);
 
 app.mount("#app");
